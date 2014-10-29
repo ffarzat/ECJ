6d8d0f7fc936132672562c4973ed734eb5080e73
ffarzat <fabiofarzat@gmail.com>
2014-10-29 12:36:23 -0200

    Identação

--- ECJ/src/ec/app/GETeste/Somar.java
+++ ECJ/src/ec/app/GETeste/Somar.java
@@ -24,26 +24,21 @@ public class Somar extends GPNode {
 	public int expectedChildren() { return 2; }
 	
 	
-	public void eval(final EvolutionState state,
-	        final int thread,
-	        final GPData input,
-	        final ADFStack stack,
-	        final GPIndividual individual,
-	        final Problem problem)
-	        {
-	        double result;
-	        DoubleData rd = ((DoubleData)(input));
+	public void eval(final EvolutionState state, final int thread, final GPData input, final ADFStack stack, final GPIndividual individual, final Problem problem)
+    {
+        double result;
+        DoubleData rd = ((DoubleData)(input));
 
-	        children[0].eval(state,thread,input,stack,individual,problem);
-	     
-	        children[1].eval(state,thread,input,stack,individual,problem);
-	        
-	        //Faz a soma
-	        rd.z = rd.x + rd.y;
-	        
-			for (int i = 0; i < 500; i++) {				
-			}
-	        
-	        }
+        children[0].eval(state,thread,input,stack,individual,problem);
+     
+        children[1].eval(state,thread,input,stack,individual,problem);
+        
+        //Faz a soma
+        rd.z = rd.x + rd.y;
+        
+		for (int i = 0; i < 500; i++) {				
+		}
+    
+    }
 
 }
